559707cbc9b199f8c05e5b8c0f1d3208f7e6fb30
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-05-05 14:16:12 +0700

    update

--- Assets/Scripts/Map1Scripts/Map2Script.cs
+++ Assets/Scripts/Map1Scripts/Map2Script.cs
@@ -14,54 +14,45 @@ public class Map2Script : MonoBehaviour {
 	}
 	
 	public void addBlockCell(float x, float y) {
-		Debug.Log("Not cool");
 		this.blockCell.Add (new MyPoint (x, y));
 	}
-	
+	//only accept vertical or horizontal line
+	private void createVerticalLineBlock(float start, float end, float hori){
+		float i = -100;
+		for (i = start; i <= end; i++) {
+			addBlockCell (hori, i);
+		}	
+	}
+
+	private void createHorizontalLineBlock(float start, float end, float vert){
+		float i = -100;
+		for (i = start; i <= end; i++) {
+			addBlockCell (i, vert);
+		}	
+	}
 	private void addBlockCell() {
-		float i = -8f;
-		float j = -4f;
-
-		Debug.Log("Cool222");
-		while(i <= 7f) {
-			while(j <= 4f) {
-				if(i == -8f || i == 7f 
-				   || i == -7f || i == 6f) {
-						addBlockCell(i, j);
-				}
-				if(j == -5f || j == 4f
-				   || j == -3f || j == 3f) {
-						addBlockCell(i, j);
-				}
-
-				j = j + 1f;
-			}
-			j = -5f;
-			i = i + 1f;
-		}
-		addBlockCell (-6f, 2f);
-		addBlockCell (-5f, 2f);
-		addBlockCell (-5f, 1f);
-		addBlockCell (-5f, 0f);
-		addBlockCell (-5f, -1f);
-		addBlockCell (-4f, -1f);
-		addBlockCell (-3f, -1f);
-		addBlockCell (-2f, -1f);
-		addBlockCell (-2f, 0f);
-		addBlockCell (-2f, 1f);
-		addBlockCell (-2f, 2f);
-		addBlockCell (-1f, 2f);
-		addBlockCell (0f, 2f);
-		addBlockCell (1f, 2f);
-		addBlockCell (1f, 1f);
-		addBlockCell (1f, 0f);
-		addBlockCell (1f, -1f);
-		addBlockCell (2f, -1f);
-		addBlockCell (3f, -1f);
+		createHorizontalLineBlock (-9f, -5f, 2f);
+		createHorizontalLineBlock (-9f, -5f, 3f);
+		createVerticalLineBlock (-3f, 3f, -6f);
+		createVerticalLineBlock (-3f, 3f, -5f);
+		createHorizontalLineBlock (-6f, -2f, -1f);
+		createHorizontalLineBlock (-6f, -2f, -2f);
+		createVerticalLineBlock (-3f, 3f, -2f);
+		createVerticalLineBlock (-3f, 3f, -3f);
+		createHorizontalLineBlock (-3f, 3f, 1f);
+		createHorizontalLineBlock (-3f, 3f, 2f);
+		createVerticalLineBlock (-3f, 3f, 1f);
+		createVerticalLineBlock (-3f, 3f, 2f);
+		createHorizontalLineBlock (2f, 6f, -1f);
+		createHorizontalLineBlock (2f, 6f, -2f);
+		createVerticalLineBlock (-2f, 1f, 5f);
+		createHorizontalLineBlock (6f, 8f, 1f);
+		addBlockCell (2f, -4f);
+		addBlockCell (7f, -3f);
 		addBlockCell (4f, -1f);
-		addBlockCell (4f, 0f);
-		addBlockCell (4f, 1f);
-		addBlockCell (5f, 1f);
-
+		addBlockCell (-4f, 1f);
+		addBlockCell (-7f, -1f);
+		addBlockCell (1f, 3f);
+		addBlockCell (7f, 2f);
 	}
 }
--- Assets/Scripts/Map1Scripts/Map3Script.cs
+++ Assets/Scripts/Map1Scripts/Map3Script.cs
@@ -24,51 +24,40 @@ public class Map3Script : MonoBehaviour {
 			}
 		}
 	}
-	
+	//only accept vertical or horizontal line
+	private void createVerticalLineBlock(float start, float end, float hori){
+		float i = -100;
+		for (i = start; i <= end; i++) {
+			addBlockCell (hori, i);
+		}	
+	}
+
+	private void createHorizontalLineBlock(float start, float end, float vert){
+		float i = -100;
+		for (i = start; i <= end; i++) {
+			addBlockCell (i, vert);
+		}	
+	}
+
 	private void addBlockCell() {
-		float i = -8f;
-		float j = -5f;
-		
-		while(i <= 7f) {
-			while(j <= 4f) {
-				if(i == -8f || i == 7f 
-				   || i == -7f || i == 6f) {
-					addBlockCell(i, j);
-				}
-				if(j == -5f || j == 4f
-				   || j == -3f || j == 3f) {
-					addBlockCell(i, j);
-				}
-				
-				j = j + 1f;
-			}
-			j = -5f;
-			i = i + 1f;
-		}
-
-
-		addBlockCell (-6f, 1f);
-		addBlockCell (-5f, 1f);
-		addBlockCell (-4f, 1f);
-		addBlockCell (-4f, 2f);
-		addBlockCell (-1f, 2f);
-		addBlockCell (-2f, 2f);
-		addBlockCell (-2f, 1f);
-		addBlockCell (-2f, 0f);
-		addBlockCell (-2f, -1f);
-		addBlockCell (-1f, -1f);
-		addBlockCell (0f, -1f);
-		addBlockCell (1f, -1f);
-		addBlockCell (1f, 0f);
-		addBlockCell (1f, 1f);
-		addBlockCell (1f, 2f);
-		addBlockCell (0f, 2f);
-
-		addBlockCell (3f, 2f);
-		addBlockCell (3f, 1f);
-		addBlockCell (4f, 1f);
-		addBlockCell (5f, 1f);
-		addBlockCell (5f, 0f);
-		
+		createHorizontalLineBlock (-7f, -4f, 1f);
+		createVerticalLineBlock (1f, 3f, -4f);
+		createHorizontalLineBlock (-4f, -1f, 3f);
+		createVerticalLineBlock (-2f, 3f, -2f);
+		createVerticalLineBlock (-2f, 3f, -1f);
+		createVerticalLineBlock (-2f, 3f, 0f);
+		createVerticalLineBlock (-2f, 3f, 1f);
+		createVerticalLineBlock (-2f, 3f, 2f);
+		createHorizontalLineBlock (1f, 4f, 3f);
+		createVerticalLineBlock (1f, 3f, 4f);
+		createHorizontalLineBlock (4f, 7f, 1f);
+		createVerticalLineBlock (-2f, 1f, 6f);
+		createVerticalLineBlock (-2f, 1f, 7f);
+		addBlockCell (-3f, -1f);
+		addBlockCell (3f, -2f);
+		addBlockCell (-5f, 2f);
+		addBlockCell (-5f, -1f);
+		addBlockCell (5f, -1f);
+		addBlockCell (5f, 3f);
 	}
 }
